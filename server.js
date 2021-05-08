79655222318eb7921ba7c41097ece56344cef284
IFramex <kulsasha55@gmail.com>
2021-05-09 00:06:50 +0300

    post put delete tasks

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get("/checkfolder/:ip", async (req, res) => {
   let ip = req.params.ip;
   if (ip == null) res.sendStatus(500)
@@ -73,6 +74,89 @@ app.post("/saveTitle", async (req, res) => {
   let newPath = `files/${req.body.ip}/${req.body.title}.json`
   fs.renameSync(path, newPath)
   res.sendStatus(200)
+})
+
+app.post("/editDescription", async (req, res) => {
+  let path = `files/${req.body.ip}/${req.body.title}.json`
+  let json = fs.readFileSync(path, "utf-8")
+  let file = JSON.parse(json)
+  file.tasks.map(task => {
+    if (task.id == req.body.taskID) task.description = req.body.value
+  })
+  fs.writeFileSync(path, JSON.stringify(file))
+  res.send(file)
+})
+
+app.post("/setTaskDone", async (req, res) => {
+  let path = `files/${req.body.ip}/${req.body.title}.json`
+  let json = fs.readFileSync(path, "utf-8")
+  let file = JSON.parse(json)
+
+  file.tasks.map(task => {
+    if (task.id == req.body.id) task.isDone = req.body.isDone
+  })
+
+  fs.writeFileSync(path, JSON.stringify(file))
+  res.sendStatus(200)
+})
+
+
+function generateID(tasksList) {
+  let words = "abcdefghijklmnopqrstuvwxyz";
+  
+  while (true) {
+    let id = ""
+    for (let i = 0; i <= 14; i++) {
+      id += words.charAt(Math.floor(Math.random() * words.length))
+    }
+    let isTask = false
+    tasksList.map(item => {
+      if (item.id == id) isTask = true
+    })
+
+    if (!isTask) return id
+  }
+}
+
+app.post("/appendTask", async (req, res) => {
+  let path = `files/${req.body.ip}/${req.body.title}.json`
+  let json = fs.readFileSync(path, "utf-8")
+  
+  let file = JSON.parse(json)
+  let id = generateID(file.tasks)
+
+  let task = { description: req.body.description, isDone: false, id, miniTasks: [] }
+  file.tasks.push(task)
+
+  fs.writeFileSync(path, JSON.stringify(file))
+  res.send(task)
+})
+
+app.delete("/deleteTask/:ip/:title/:taskID", async (req, res) => {
+  let path = `files/${req.params.ip}/${req.params.title}.json`
+  let json = fs.readFileSync(path, "utf-8")
+  let file = JSON.parse(json)
+  file.tasks.map((task, index) => {
+    if (task.id == req.params.taskID) file.tasks.splice(index, 1)
+  })
+
+  fs.writeFileSync(path, JSON.stringify(file))
+  res.sendStatus(200)
+})
+
+app.post("/saveMiniTask", async (req, res) => {
+  let path = `files/${req.body.ip}/${req.body.title}.json`
+  let json = fs.readFileSync(path, "utf-8")
+  let file = JSON.parse(json)
+  let id = generateID(file.tasks)
+
+  let newTask = { description: req.body.taskText, isDone: false, id, miniTasks: [] }
+  file.tasks.map((task, index) => {
+    if (task.id == req.body.parentID) file.tasks[index].miniTasks.push(newTask)
+  })
+
+  fs.writeFileSync(path, JSON.stringify(file))
+  res.send(newTask)
 })
 
 app.listen(3001, () => console.log("server is started on 3001"));
